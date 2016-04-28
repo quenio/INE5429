412c1ec4f89a548f43efb7beccc2cb0d6a7bf53a
Quenio dos Santos <quenio@me.com>
2016-04-28 17:21:22 -0300

    Using GMP.

--- 04-29/main.c
+++ 04-29/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <gmp.h>
+
+const int base = 10;
 
 int main()
 {
-    printf("Hello, World!");
-
+    mpz_t pie;
+    mpz_init_set_str(pie, "3141592653589793238462643383279502884", base);
+    mpz_add_ui(pie, pie, 1);
+    mpz_out_str(NULL, base, pie);
+    mpz_clear(pie);
     return 0;
 }
