9875fb193224109a993da6a85494246e0abe83f1
Quenio dos Santos <quenio@me.com>
2016-07-04 11:09:45 -0300

    Implemented the rho transformation.

--- TG/SHA-3/rho.h
+++ TG/SHA-3/rho.h
@@ -3,9 +3,6 @@
 #pragma once
 
 #include "state_array.h"
-#include <iostream>
-
-using namespace std;
 
 template<size_t W>
 StateArray<W> rho(StateArray<W> & a)
@@ -23,12 +20,12 @@ StateArray<W> rho(StateArray<W> & a)
     Coord3D coord { 1, 0, 0 };
     for (int t = 0; t < 24; t++)
     {
-        cout << "coord = (" << coord.x << "," << coord.y << ")" << endl;
-
         for (;coord.z < SA::lane_size; coord.z++)
         {
             const int offset_z = (coord.z - (t+1)*(t+2)/2) % SA::lane_size;
             const Coord3D offset { coord.x, coord.y, offset_z };
+
+            b.set(coord, a[offset]);
         }
         coord = Coord3D { coord.y, (2 * coord.x + 3 * coord.y) % 5, 0 };
     }
--- TG/SHA-3/rho_tests.cpp
+++ TG/SHA-3/rho_tests.cpp
@@ -3,10 +3,8 @@
 #include "min_unit.h"
 #include "rho.h"
 
-using namespace std;
-
 const char *s1 = "01100110011001100110011001100110011001100110011001";
-const char *s2 = "01011001011010011010010110010110100110100101100101";
+const char *s2 = "01010110101001101010101010011001100110100110101001";
 
 using SA2 = StateArray<2>;
 using BS50 = BitString<50>;
@@ -14,11 +12,8 @@ using BS50 = BitString<50>;
 const char * rho_test()
 {
     SA2 a { BS50 { s1 } };
-
     SA2 b = rho(a);
-
     mu_assert(b.to_string() == s2);
-
     return NULL;
 }
 
