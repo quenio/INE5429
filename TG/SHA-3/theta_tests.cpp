f274f6241a7d15c4aeaa900fd6108867f169aef0
Quenio dos Santos <quenio@me.com>
2016-07-05 22:36:06 -0300

    Verified 64-bit lanes.

--- TG/SHA-3/theta_tests.cpp
+++ TG/SHA-3/theta_tests.cpp
@@ -2,19 +2,19 @@
 
 #include "min_unit.h"
 #include "theta.h"
+#include "samples.h"
 
 using namespace std;
 
-const char *s1 = "01100110011001100110011001100110011001100110011001";
-const char *s2 = "01011001011010011010010110010110100110100101100101";
-
-using SA2 = StateArray<2>;
-using BS50 = BitString<50>;
-
-const char * theta_test()
+const char * theta_SA2()
 {
-    SA2 a { BS50 { s1 } };
+    using SA2 = StateArray<2>;
+    using BS50 = BitString<50>;
 
+    const char *s1 = "01100110011001100110011001100110011001100110011001";
+    const char *s2 = "01011001011010011010010110010110100110100101100101";
+
+    SA2 a { BS50 { s1 } };
     SA2 b = theta(a);
 
     mu_assert(b.to_string() == s2);
@@ -22,7 +22,20 @@ const char * theta_test()
     return NULL;
 }
 
+const char * theta_SA64()
+{
+    using SA64 = StateArray<64>;
+
+    SA64 a { hex_to_bs<1600>(MSG5_XOR) };
+    SA64 b = theta(a);
+
+    mu_assert(b.to_hex() == MSG5_THETA);
+
+    return NULL;
+}
+
 void all_tests()
 {
-    mu_test(theta_test);
+    mu_test(theta_SA2);
+    mu_test(theta_SA64);
 }
